bd5697c9855c3b2ca7706b5e44708bead933b19a
Akongstad <kongstad@outlook.com>
2021-10-01 13:20:57 +0200

    Co-authored-by: Benjamin-von-Barner-Altenburg <Benjamin-von-Barner-Altenburg@users.noreply.github.com>Setup calasses

--- Assignment4.Core/State.cs
+++ Assignment4.Core/State.cs
@@ -1,6 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment4.Core
 {
+    [Required]
     public enum State
     {
+        
+        New,
+        Active,
+        Resolved,
+        Closed,
+        Removed
+        
     }
 }
