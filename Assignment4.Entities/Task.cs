3160eaba8826b3e541f40d672929351bec7bbf38
Akongstad <90042173+Akongstad@users.noreply.github.com>
2021-10-01 13:22:35 +0200

    Co-authored-by: Benjamin-von-Barner-Altenburg <Benjamin-von-Barner-Altenburg@users.noreply.github.com>Setup calasses (#1)

--- Assignment4.Entities/Task.cs
+++ Assignment4.Entities/Task.cs
@@ -1,6 +1,23 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment4.Entities
 {
     public class Task
     {
+        public Task(){
+            this.tags = new HashSet<Tag>();
+        }
+        public int iD {get; set;}
+
+        [StringLength(100)]
+        [Required]
+        public string Title {get; set;}
+        public int? userID {get; set;}
+        public string? Description {get; set;}
+        public enum State {get; set;}
+        public ICollection<Tag> tags{get; set;}
     }
+
+    
 }
--- Assignment4.Entities/User.cs
+++ Assignment4.Entities/User.cs
@@ -1,6 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Assignment4.Entities
 {
     public class User
     {
+        public int iD { get; set; }
+
+        [StringLength(100)]
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength(100)]
+        [Required]
+        [EmailAddress]
+        [Remote(action: "VerifyEmail", controller: "UserController",ErrorMessage="Email already in use")]
+        public string Email { get; set; }
+
+        public ICollection<int> Tasks { get; set; }
     }
 }
