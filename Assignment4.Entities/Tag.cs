ee596aa265e96790fd8de5a091df754022da391d
Akongstad <kongstad@outlook.com>
2021-10-01 13:25:53 +0200

    tag

--- Assignment4.Entities/Tag.cs
+++ Assignment4.Entities/Tag.cs
@@ -1,6 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment4.Entities
 {
     public class Tag
     {
+       public int id {get; set; }
+
+       [Required]
+       [StringLength(50)]
+       public string name{get; set;} 
     }
 }
